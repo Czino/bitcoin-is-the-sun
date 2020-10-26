fa610be88ba45b41ffdca3758fa5700d40b4b42e
Eric Lehmann <eric@machinas.com>
2020-10-26 21:38:35 +0100

    Delete config.py (I created new keys, so ..)

--- config.py
+++
@@ -1,6 +0,0 @@
-credentials = {
-    "consumer_key": "WXzAax1qyOYBWt1WKKmoX1Rwr",
-    "consumer_secret": "ydcz287mE0La2tP9dCjhwPnsGR7FlvT8mKdV7d9sMCc7M1uemH",
-    "access_token": "2897722599-AlKYK5fb8Wl4M4WL6f5ntXLqsTw5TJXBSfNHqkA",
-    "access_token_secret": "3p25D8rqwQ8m8zVINZChIUVBuMHxzyV9IW1nBKbMn7MxM"
-}
